22f156e22b779839b2729f243b5a3f918621e7c0
mtouma56 <mtouma56@gmail.com>
2025-10-04 18:39:46 +0000

    fix: render homepage at root route (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { useEffect } from "react";
-import Index from "./pages/Index";
+import HomePage from "./pages/HomePage";
 import AuditReport from "./pages/AuditReport";
 import NotFound from "./pages/NotFound";
 import { ScrollToTop } from "./components/ui/ScrollToTop";
@@ -50,7 +50,7 @@ const App = () => {
           <BrowserRouter>
             <ScrollToTop />
             <Routes>
-              <Route path="/" element={<Index />} />
+              <Route path="/" element={<HomePage />} />
               <Route path="/audit" element={<AuditReport />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
